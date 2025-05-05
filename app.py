b646f5b0c7f032b46172de3ab7f2dfcd8184a669
Rashad Malik <rashad.malik@iic.ac.in>
2025-05-05 15:09:02 +0530

    u

--- app.py
+++ app.py
@@ -56,5 +56,8 @@ def predict():
     prediction = model.predict(input_df)
     return jsonify({'prediction': float(prediction[0])})
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local use
+    app.run(host='0.0.0.0', port=port)
